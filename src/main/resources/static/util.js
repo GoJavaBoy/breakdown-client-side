dbaa08e956fbcfef62787dea24d1360dfbcb8c06
Stasis Yukniavichus <stasys1996@gmail.com>
2021-02-04 08:50:49 +0000

    Last commit before Intership, last point in personal_details, button

--- src/main/resources/static/util.js
+++ src/main/resources/static/util.js
@@ -86,7 +86,7 @@ function getPersonalDetails() {
 
     var xhr2 = new XMLHttpRequest();
     xhr2.open('GET', 'https://api.telegram.org/bot1684613071:AAGYMCdPLof7JB_U0hKtM1psvKLhhqejuZE/sendLocation?chat_id=@emergency_breakdown&latitude='
-         + localStorage.getItem("latitude") + '&longitude=' + localStorage.getItem("longtude"), false);
+        + localStorage.getItem("latitude") + '&longitude=' + localStorage.getItem("longtude"), false);
     xhr2.send();
 
     // xhr.onload = function () {
@@ -96,15 +96,49 @@ function getPersonalDetails() {
 }
 
 
-    $(document).ready(function() {
+$(document).ready(function () {
     function progress(timeleft, timetotal, $element) {
         var progressBarWidth = timeleft * $element.width() / timetotal;
-        $element.find('div').animate({ width: progressBarWidth }, timeleft === timetotal ? 0 : 1000, 'linear').html("paskambinsime po " + timeleft);
-        if(timeleft > 0) {
-            setTimeout(function() {
+        $element.find('div').animate({width: progressBarWidth}, timeleft === timetotal ? 0 : 1000, 'linear').html("paskambinsime po " + timeleft);
+        if (timeleft > 0) {
+            setTimeout(function () {
                 progress(timeleft - 1, timetotal, $element);
             }, 1000);
         }
     }
+
     progress(180, 180, $('#progressBar'));
 });
+
+function validPhone() {
+    let inputs = document.querySelectorAll('input[data-rules]');
+    for (let input of inputs) {
+        input.addEventListener('blur', function () {
+            let rules = this.dataset.rules;
+            let value = this.value;
+            let check;
+            switch (rules) {
+                case 'tel':
+                    check = /^\d+$/.test(value);
+                    break;
+            }
+            if (check) {
+                this.classList.remove('invalid');
+                this.classList.add('valid');
+            } else {
+                this.classList.remove('valid');
+                this.classList.add('invalid');
+            }
+        })
+    }
+}
+
+function ValidPhone() {
+    var re = /^\d[\d\(\)\ -]{4,14}\d$/;
+    var myPhone = document.getElementById('phone-number').value;
+    var valid = re.test(myPhone);
+    if (valid) output = 'Номер телефона введен правильно!';
+    else alert("ADASd")// output = 'Номер телефона введен неправильно!';
+    document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
+    return valid;
+}
